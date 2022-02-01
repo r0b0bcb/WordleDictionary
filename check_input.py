3d62bb8280ed6e195c86bc9b8efdbce88a4890a9
B.Clay Barlow <36454538+r0b0bcb@users.noreply.github.com>
2022-02-01 09:47:37 -0500

    Updated yellow search check for yellow_search2 format

--- check_input.py
+++ check_input.py
@@ -1,9 +1,9 @@
 def check_input(green, yellow, exclude):
-    if len(green) != 5 or len(yellow) > 5:
+    if len(green) != 5 or len(yellow) != 5:
         return False
     elif all(x.isalpha() for x in green.translate(str.maketrans('','','?'))) != True:
         return False
-    elif all(x.isalpha() for x in yellow) != True:
+    elif all(x.isalpha() for x in yellow.translate(str.maketrans('','','?'))) != True:
         return False
     elif all(x.isalpha() for x in exclude) != True:
         return False
